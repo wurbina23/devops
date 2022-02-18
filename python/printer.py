9c05b40e130311f7f9a6b4a5a8e396bddc155af4
Walter Urbina <walter_urbina@outlook.com>
2022-02-18 18:17:54 -0500

    adding new files

---
+++ python/printer.py
@@ -0,0 +1,6 @@
+def lambda_handler(event, context):
+    print(event)
+    return event
+
+#understandin-24-2ee59e-scope-lab
+#serviceaccount is the equivalent to an ec2 role that has access to s3 bucket in aws 
